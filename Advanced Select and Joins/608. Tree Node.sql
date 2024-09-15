4cce95817a59164c6e994ef73546f44009cc0456
sukhjotk13 <sukhjotk29@gmail.com>
2024-09-14 21:12:54 -0400

    Create 608. Tree Node.sql

---
+++ Advanced Select and Joins/608. Tree Node.sql
@@ -0,0 +1,8 @@
+SELECT 
+    id,
+    CASE 
+        WHEN p_id IS NULL THEN 'Root'               
+        WHEN id NOT IN (SELECT DISTINCT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Leaf'-- Leaf node condition
+        ELSE 'Inner'                              
+    END AS type
+FROM Tree;
