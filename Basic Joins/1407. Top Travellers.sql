99f023a71f853ce663045b6e530628fb706f334f
sukhjotk13 <sukhjotk29@gmail.com>
2024-09-14 20:53:11 -0400

    Create 1407. Top Travellers.sql

---
+++ Basic Joins/1407. Top Travellers.sql
@@ -0,0 +1,9 @@
+SELECT 
+    u.name AS name,
+    COALESCE(SUM(r.distance), 0) AS travelled_distance
+FROM
+    Users u
+        LEFT JOIN
+    Rides r ON (u.id = r.user_id)
+GROUP BY u.id
+ORDER BY travelled_distance DESC , u.name;
